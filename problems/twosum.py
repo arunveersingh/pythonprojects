1fd5bb2ae4e8ec469de512e2df96c3cbcc693637
arunveersingh <arunveersingh9@gmail.com>
2020-04-29 19:29:34 +0530

    Two Sum Problem

--- problems/twosum.py
+++ problems/twosum.py
@@ -10,17 +10,29 @@
 # return [0, 1].
 from typing import List
 
+
 class Solution:
     @staticmethod
-    def two_sum(nums: List[int], target: int) -> List[List]:
+    def two_sum(nums: List[int], target: int) -> List[List[int]]:
         number_map = {number: number for number in nums}
         results = []
         for number in nums:
             if (target - number) in number_map:
                 results.append([number, (target - number)])
-                del(number_map[number])
+                del (number_map[number])
 
         return results
+
+class SolutionX:
+    @staticmethod
+    def two_sum(nums: List[int], target: int) -> List[int]:
+        values = {}
+        counter = 0
+        for number in nums:
+            if target - number in values.keys():
+                return [values.get(target - number), counter]
+            values[number] = counter
+            counter += 1
 
 
 result = Solution.two_sum([11, 7, 15, 2], 9)
@@ -28,3 +40,12 @@ print(result)
 
 result = Solution.two_sum([11, 7, 15, 2], 19)
 print(result)
+
+result = Solution.two_sum([3, 2, 1], 6)
+print(result)
+
+result = SolutionX.two_sum([3, 2, 4], 6)
+print("found: ", result)
+
+result = SolutionX.two_sum([3, 3], 6)
+print("found: ", result)
